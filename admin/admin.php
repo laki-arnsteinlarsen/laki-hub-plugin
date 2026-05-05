a16576b86ee9d89bf64d3a3babb34fb4bfc184f5
laki-arnsteinlarsen <arnstein.larsen@laki.no>
2026-05-05 14:51:11 +0200

    feat: add Produkter submenu item

--- admin/admin.php
+++ admin/admin.php
@@ -17,11 +17,12 @@ class Edifice_Admin {
             'dashicons-building',
             3
         );
-        add_submenu_page('edifice', 'Dashbord',       'Dashbord',       'manage_options', 'edifice',              [__CLASS__, 'page_dashboard']);
-        add_submenu_page('edifice', 'CRM',            'CRM',            'manage_options', 'edifice-crm',          [__CLASS__, 'page_crm']);
-        add_submenu_page('edifice', 'Prosjekter',     'Prosjekter',     'manage_options', 'edifice-projects',     [__CLASS__, 'page_projects']);
-        add_submenu_page('edifice', 'Timeføring',     'Timeføring',     'manage_options', 'edifice-time',         [__CLASS__, 'page_time']);
-        add_submenu_page('edifice', 'Inntekt',        'Inntekt',        'manage_options', 'edifice-revenue',      [__CLASS__, 'page_revenue']);
+        add_submenu_page('edifice', 'Dashbord',           'Dashbord',           'manage_options', 'edifice',              [__CLASS__, 'page_dashboard']);
+        add_submenu_page('edifice', 'CRM',                'CRM',                'manage_options', 'edifice-crm',          [__CLASS__, 'page_crm']);
+        add_submenu_page('edifice', 'Prosjekter',         'Prosjekter',         'manage_options', 'edifice-projects',     [__CLASS__, 'page_projects']);
+        add_submenu_page('edifice', 'Timeføring',         'Timeføring',         'manage_options', 'edifice-time',         [__CLASS__, 'page_time']);
+        add_submenu_page('edifice', 'Inntekt',            'Inntekt',            'manage_options', 'edifice-revenue',      [__CLASS__, 'page_revenue']);
+        add_submenu_page('edifice', 'Produkter',          'Produkter',          'manage_options', 'edifice-products',     [__CLASS__, 'page_products']);
     }
 
     public static function enqueue_assets(string $hook) {
@@ -39,4 +40,5 @@ class Edifice_Admin {
     public static function page_projects()  { include EDIFICE_DIR . 'admin/views/projects.php'; }
     public static function page_time()      { include EDIFICE_DIR . 'admin/views/time.php'; }
     public static function page_revenue()   { include EDIFICE_DIR . 'admin/views/revenue.php'; }
+    public static function page_products()  { include EDIFICE_DIR . 'admin/views/products.php'; }
 }
